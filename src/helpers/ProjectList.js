13ed964ddf726575a6f3ee6c3941ca22d69a9809
Muthuk07 <muthukesavan6044@gmail.com>
2023-08-15 21:07:32 +0530

    main

--- src/helpers/ProjectList.js
+++ src/helpers/ProjectList.js
@@ -1,17 +1,29 @@
 import Proj1 from "../assets/proj1.png";
 import Proj2 from "../assets/proj2.jpg";
+import proj3 from "../assets/proj3.png";
+import proj4 from "../assets/proj4.jpg";
 
 
 export const ProjectList = [
   {
     name: "Weather Website",
     image: Proj1,
-    skills: "JavaScript,HTML,CSS",
+    skills: "HTML,CSS,Javascript, API.",
   },
   {
     name: "Instagram Reels Downloader",
     image: Proj2,
-    skills: "React,API",
+    skills: "React,API.",
+  },
+  {
+    name: "Chat application for SRM students(UNDER DEVELOPING)",
+    image: proj3,
+    skills: "React, Node, Express, MongoDB."
+  },
+  {
+    name: "File Integrity Monitoring system",
+    image: proj4,
+    skills: "Powershell"
   },
 ];
   
